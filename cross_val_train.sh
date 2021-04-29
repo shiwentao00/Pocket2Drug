7b44706f0cec486891a121a4d63974d345e6747c
Wentao Shi <wentao771@gmail.com>
2021-04-29 12:36:17 -0500

    cross validation train script

--- cross_val_train.sh
+++ cross_val_train.sh
@@ -7,9 +7,9 @@
 
 cd /work/derick/siamese-monet-project/Pocket2Drug/
 
-singularity exec --nv -B /work,/project,/usr/lib64 /work/derick/singularities/pytorch171.simg python cross_validation_train.py -val_fold 0 -out_dir ../p2d_results/cross_val_fold_0/ &> ./cross_validation/train_logs/train_fold0.log 2>&1
-singularity exec --nv -B /work,/project,/usr/lib64 /work/derick/singularities/pytorch171.simg python cross_validation_train.py -val_fold 1 -out_dir ../p2d_results/cross_val_fold_1/ &> ./cross_validation/train_logs/train_fold1.log 2>&1
-singularity exec --nv -B /work,/project,/usr/lib64 /work/derick/singularities/pytorch171.simg python cross_validation_train.py -val_fold 2 -out_dir ../p2d_results/cross_val_fold_2/ &> ./cross_validation/train_logs/train_fold2.log 2>&1
-singularity exec --nv -B /work,/project,/usr/lib64 /work/derick/singularities/pytorch171.simg python cross_validation_train.py -val_fold 3 -out_dir ../p2d_results/cross_val_fold_3/ &> ./cross_validation/train_logs/train_fold3.log 2>&1
-singularity exec --nv -B /work,/project,/usr/lib64 /work/derick/singularities/pytorch171.simg python cross_validation_train.py -val_fold 4 -out_dir ../p2d_results/cross_val_fold_4/ &> ./cross_validation/train_logs/train_fold4.log 2>&1
+singularity exec --nv -B /work,/project,/usr/lib64 /work/derick/singularities/pytorch171.simg python cross_val_train.py -val_fold 0 -out_dir ../p2d_results/cross_val_fold_0/ &> ./cross_validation/train_logs/train_fold0.log 2>&1
+singularity exec --nv -B /work,/project,/usr/lib64 /work/derick/singularities/pytorch171.simg python cross_val_train.py -val_fold 1 -out_dir ../p2d_results/cross_val_fold_1/ &> ./cross_validation/train_logs/train_fold1.log 2>&1
+singularity exec --nv -B /work,/project,/usr/lib64 /work/derick/singularities/pytorch171.simg python cross_val_train.py -val_fold 2 -out_dir ../p2d_results/cross_val_fold_2/ &> ./cross_validation/train_logs/train_fold2.log 2>&1
+singularity exec --nv -B /work,/project,/usr/lib64 /work/derick/singularities/pytorch171.simg python cross_val_train.py -val_fold 3 -out_dir ../p2d_results/cross_val_fold_3/ &> ./cross_validation/train_logs/train_fold3.log 2>&1
+singularity exec --nv -B /work,/project,/usr/lib64 /work/derick/singularities/pytorch171.simg python cross_val_train.py -val_fold 4 -out_dir ../p2d_results/cross_val_fold_4/ &> ./cross_validation/train_logs/train_fold4.log 2>&1
 
