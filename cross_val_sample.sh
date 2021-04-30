ec9e1cd2237e7a13137462900309777b8cf00132
Wentao Shi <wentao771@gmail.com>
2021-04-30 15:36:19 -0500

    sampling batch script

--- cross_val_sample.sh
+++ cross_val_sample.sh
@@ -7,9 +7,9 @@
 
 cd /work/derick/siamese-monet-project/Pocket2Drug/
 
-singularity exec --nv -B /work,/project,/usr/lib64 /work/derick/singularities/pytorch171.simg python cross_val_sample.py -fold 5 -result_dir ../p2d_results/cross_val_fold_5/ &> ./cross_validation/sample_logs/sample_fold5.log 2>&1
-singularity exec --nv -B /work,/project,/usr/lib64 /work/derick/singularities/pytorch171.simg python cross_val_sample.py -fold 6 -result_dir ../p2d_results/cross_val_fold_6/ &> ./cross_validation/sample_logs/sample_fold6.log 2>&1
-singularity exec --nv -B /work,/project,/usr/lib64 /work/derick/singularities/pytorch171.simg python cross_val_sample.py -fold 7 -result_dir ../p2d_results/cross_val_fold_7/ &> ./cross_validation/sample_logs/sample_fold7.log 2>&1
-singularity exec --nv -B /work,/project,/usr/lib64 /work/derick/singularities/pytorch171.simg python cross_val_sample.py -fold 8 -result_dir ../p2d_results/cross_val_fold_8/ &> ./cross_validation/sample_logs/sample_fold8.log 2>&1
-singularity exec --nv -B /work,/project,/usr/lib64 /work/derick/singularities/pytorch171.simg python cross_val_sample.py -fold 9 -result_dir ../p2d_results/cross_val_fold_9/ &> ./cross_validation/sample_logs/sample_fold9.log 2>&1
+singularity exec --nv -B /work,/project,/usr/lib64 /work/derick/singularities/pytorch171.simg python cross_val_sample.py -fold 0 -result_dir ../p2d_results/cross_val_fold_0/ &> ./cross_validation/sample_logs/sample_fold0.log 2>&1
+singularity exec --nv -B /work,/project,/usr/lib64 /work/derick/singularities/pytorch171.simg python cross_val_sample.py -fold 1 -result_dir ../p2d_results/cross_val_fold_1/ &> ./cross_validation/sample_logs/sample_fold1.log 2>&1
+singularity exec --nv -B /work,/project,/usr/lib64 /work/derick/singularities/pytorch171.simg python cross_val_sample.py -fold 2 -result_dir ../p2d_results/cross_val_fold_2/ &> ./cross_validation/sample_logs/sample_fold2.log 2>&1
+singularity exec --nv -B /work,/project,/usr/lib64 /work/derick/singularities/pytorch171.simg python cross_val_sample.py -fold 3 -result_dir ../p2d_results/cross_val_fold_3/ &> ./cross_validation/sample_logs/sample_fold3.log 2>&1
+singularity exec --nv -B /work,/project,/usr/lib64 /work/derick/singularities/pytorch171.simg python cross_val_sample.py -fold 4 -result_dir ../p2d_results/cross_val_fold_4/ &> ./cross_validation/sample_logs/sample_fold4.log 2>&1
 
