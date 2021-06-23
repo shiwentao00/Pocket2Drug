c8f523d298bf4eb5a8e101dc4a6c84474159120f
Wentao Shi <wentao771@gmail.com>
2021-06-22 21:53:09 -0500

    chmod+x smina_model_dock.sh

--- cross_validation/docking/docking_script/smina_model_dock.sh
+++ cross_validation/docking/docking_script/smina_model_dock.sh
@@ -2,14 +2,13 @@
 fold=0
 start=0
 #total=4836
-total = 49
+total=49
 while (($start < $total))
 do
     end=$((start + 50))
     if (($end >= $total)); then
         end=$((total - 1))
     fi
-
     job_name="dock_${fold}_${start}_${end}"
     #echo $job_name
     qsub -N ${job_name} -v FOLD=${fold},START=${start},END=${end} smina_dock.pbs
